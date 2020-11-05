e761c9f252e72c1ee2287d02d1b754b7ab132ed7
Luciano Santos <luciano_z7@hotmail.com>
2020-11-04 21:36:16 -0300

    [Feature] Adding login request

---
+++ src/controllers/login_controller.py
@@ -0,0 +1,26 @@
+import aiohttp
+import os
+
+async def fetch(session, url, data=None):
+    header = {
+        'Accept': 'application/json'
+    }
+    async with session.post(
+        url,
+        json=data,
+        headers=header
+    ) as response:
+        return await response.json(), response.status
+
+async def post_login(login):
+    response = dict()
+    status = 404
+
+    async with aiohttp.ClientSession() as session:
+        response, status = await fetch(
+            session,
+            urlEnv,
+            support
+        )
+
+    return response, status
